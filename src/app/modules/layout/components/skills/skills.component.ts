27b4a4b2abb5ed88a5fcbcdb9ad1b9819c763583
Venkatesh <venkatesh.bhonagiri10@gmail.com>
2024-10-13 13:11:04 +0530

    updated skills component

--- src/app/modules/layout/components/skills/skills.component.ts
+++ src/app/modules/layout/components/skills/skills.component.ts
@@ -7,43 +7,65 @@ import { ISkills } from '../../models/skills';
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent {
-  skills: ISkills[] = [
-    {
-      imagePath: '/assets/images/html-icon.svg',
-      name: 'HTML',
-      alt: 'Html Icon',
-    },
-    {
-      imagePath: '/assets/images/css-icon.svg',
-      name: 'CSS',
-      alt: 'Css Icon',
-    },
-    {
-      imagePath: '/assets/images/sass-icon.svg',
-      name: 'SASS',
-      alt: 'Sass Icon',
-    },
-    {
-      imagePath: '/assets/images/js-icon.svg',
-      name: 'Javascript',
-      alt: 'Javascript Icon',
-    },
-    {
-      imagePath: '/assets/images/ts-icon.svg',
-      name: 'Typescript',
-      alt: 'Typescript Icon',
-    },
-    {
-      imagePath: '/assets/images/angular-icon.svg',
-      name: 'Angular',
-      alt: 'Angular Icon',
-    },
-    {
-      imagePath: '/assets/images/react-icon.svg',
-      name: 'React',
-      alt: 'React Icon',
-    },
-  ];
+  // skills: ISkills[] = [
+  //   {
+  //     imagePath: '/assets/images/html-iconUrl.svg',
+  //     name: 'HTML',
+  //     alt: 'Html iconUrl',
+  //   },
+  //   {
+  //     imagePath: '/assets/images/css-iconUrl.svg',
+  //     name: 'CSS',
+  //     alt: 'Css iconUrl',
+  //   },
+  //   {
+  //     imagePath: '/assets/images/sass-iconUrl.svg',
+  //     name: 'SASS',
+  //     alt: 'Sass iconUrl',
+  //   },
+  //   {
+  //     imagePath: '/assets/images/js-iconUrl.svg',
+  //     name: 'Javascript',
+  //     alt: 'Javascript iconUrl',
+  //   },
+  //   {
+  //     imagePath: '/assets/images/ts-iconUrl.svg',
+  //     name: 'Typescript',
+  //     alt: 'Typescript iconUrl',
+  //   },
+  //   {
+  //     imagePath: '/assets/images/angular-iconUrl.svg',
+  //     name: 'Angular',
+  //     alt: 'Angular iconUrl',
+  //   },
+  //   {
+  //     imagePath: '/assets/images/react-iconUrl.svg',
+  //     name: 'React',
+  //     alt: 'React iconUrl',
+  //   },
+  // ];
+
+   skillIcons = [
+    { iconUrl: '/assets/skills/Java.svg', title: "Java" },
+    { iconUrl: '/assets/skills/HTML.svg', title: "HTML5" },
+    { iconUrl: '/assets/skills/CSS.svg', title: "CSS3" },
+    { iconUrl: '/assets/skills/JavaScript.svg', title: "JavaScript" },
+    { iconUrl: '/assets/skills/TypeScript.svg', title: "TypeScript" },
+    { iconUrl: '/assets/skills/MySQL.svg', title: "MySQL" },
+    { iconUrl: '/assets/skills/MongoDB.svg', title: "MongoDB" },
+    { iconUrl: '/assets/skills/Express.svg', title: "Express.js" },
+    { iconUrl: '/assets/skills/React.svg', title: "React" },
+    { iconUrl: '/assets/skills/Node.svg', title: "Node.js" },
+    { iconUrl: '/assets/skills/Nest.svg', title: "Nest.js" },
+    { iconUrl: '/assets/skills/Next.svg', title: "Next.js" },
+    { iconUrl: '/assets/skills/Three.svg', title: "Three.js" },
+    { iconUrl: '/assets/skills/Redux.svg', title: "Redux" },
+    { iconUrl: '/assets/skills/TailwindCSS.svg', title: "Tailwind CSS" },
+    { iconUrl: '/assets/skills/NumPy.svg', title: "NumPy" },
+    { iconUrl: '/assets/skills/Pandas.svg', title: "Pandas" },
+    { iconUrl: '/assets/skills/OpenCV.svg', title: "OpenCV" },
+    { iconUrl: '/assets/skills/Github.svg', title: "GitHub" },
+];
   constructor() {}
 
   ngOnInit(): void {}
